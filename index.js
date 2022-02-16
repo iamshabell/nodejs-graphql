99a55cad7930dd276ee933180f7afcb7891cc76a
iamshabell <91321698+iamshabell@users.noreply.github.com>
2022-02-16 11:57:49 +0300

    added some feature like findById

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 
 
 const {ApolloServer, gql} = require('apollo-server');
-
-const employees = require('./data/employees.json')
+const EmployeeService = require('./datasources/file.js');
 
 
 const typeDefs = gql`
     type Query {
-        employees: [Employee]
+        employees (
+        id: ID
+        firstName: String,
+        lastName: String,
+        designation: String,
+        department: String,
+        nearestCity: String
+        ): [Employee],
+        findEmployeeById(id: ID): Employee
     }
     type Employee {
-        id: ID!
+        id: ID! 
         firstName: String,
         lastName: String,
         designation: String,
@@ -19,15 +26,26 @@ const typeDefs = gql`
     }
 `
 
+
+
+const dataSources = () => ({
+    employeeService: new EmployeeService()
+}); 
+
+
 const resolvers = {
     Query: {
-        employees: () => {
-            return employees
+        employees: (parent,args,{dataSources},info) => {
+            return dataSources.employeeService.getEmployees(args);
+        },
+        findEmployeeById: (parent, {id},{dataSources},info) => {
+            return dataSources.employeeService.getEmployeesById(id)[0];
         }
     }
 }
 
-const gqlServer = new ApolloServer({typeDefs, resolvers});
+
+const gqlServer = new ApolloServer({typeDefs, resolvers,dataSources});
 
 gqlServer.listen({port: process.env.PORT || 4000})
 .then(({url}) => console.log(`graphQL server started on ${url}`))
